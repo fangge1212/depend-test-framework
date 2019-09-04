2f5538983b5e02bc3c7de78c3d677010d45ad1f5
Fangge Jin <fjin@redhat.com>
2019-09-04 09:50:47 +0800

    Draft migration.py
    Signed-off-by: Fangge Jin <fjin@redhat.com>

--- examples/migration.py
+++ examples/migration.py
@@ -19,3 +19,41 @@ def migrate(params, env):
         params.logger.info("Mock: " + cmd)
         return
     run_cmd(cmd)
+
+class migrate(TestObject):
+    """ migrate guest"""
+    _test_entry = set([Action(1),
+                       ParamsRequire(['guest_name', 'target_host'])])
+    def __init__(self):
+        self._test_entry.add(Consumer('$target_host.$guest_name.active', Consumer.REQUIRE_N))
+
+class offline_migrate(migrate):
+    """ Offline migrate guest"""
+    def __init__(self):
+        self.test_entry.add(Consumer('$guest_name.active|$guest_name.config.', Consumer.REQUIRE))
+        self.test_entry.add(Provider('$target_host.$guest_name.config', Provider.SET))
+
+    def __call__(self, params, env):
+        target_host = params.target_host
+        guest_name = params.guest_name
+        protocol = params.protocol if params.protocol else 'ssh'
+
+        cmd = 'virsh migrate %s qemu+%s://%s/system --offline' % (guest_name, protocol, target_host)
+        ret = run_cmd(cmd)
+        params.logger.info("# %s\n%s", cmd, ret)
+
+class live_migrate(migrate):
+    """ Live migrate guest"""
+    def __init__(self):
+        self._test_entry.add(Consumer('$guest_name.active', Consumer.REQUIRE)) 
+        self._test_entry.add(Provider('$target_host.$guest_name.active', Provider.SET)) 
+        self._test_entry.add(Provider('$guest_name.active', Provider.CLEAR)) 
+
+    def __call__(self, params, env):
+        target_host = params.target_host
+        guest_name = params.guest_name
+        protocol = params.protocol if params.protocol else 'ssh'
+
+        cmd = 'virsh migrate %s qemu+%s://%s/system --live' % (guest_name, protocol, target_host)
+        ret = run_cmd(cmd)
+        params.logger.info("# %s\n%s", cmd, ret)
